8681f0661b0414a6d8fabde8ef3f80569456389a
pooyansb <79424335+pooyansb@users.noreply.github.com>
2025-07-31 11:14:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ def create_app():
 
     return app
 
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=False)
